2a74dc1278392e99f0b5a57d7f13cf8841427af4
rblcoder <35212670+rblcoder@users.noreply.github.com>
2023-02-25 18:42:52 +0530

    Added views

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'products'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('categories', views.categories, name='categories'),
+    path('currencies', views.currencies, name='currencies'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- products/views.py
+++ products/views.py
@@ -7,4 +7,16 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse("Hello, world. You are in products index")
-    return render(request, 'products/product_list.html', {})
+    return render(request, 'products/index.html', {})
+
+def categories(request):
+    # return HttpResponse("Hello, world. You are in products index")
+    return render(request, 'products/categories.html', {})
+
+def currencies(request):
+    # return HttpResponse("Hello, world. You are in products index")
+    return render(request, 'products/currencies.html', {})
+
+def dashboard(request):
+    # return HttpResponse("Hello, world. You are in products index")
+    return render(request, 'products/dashboard.html', {})
