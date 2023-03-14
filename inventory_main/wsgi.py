654b2c8262e9ce1682943d9c4ac5c6d2cb9d5775
rblcoder <35212670+rblcoder@users.noreply.github.com>
2023-03-14 19:45:20 +0530

    revert changes to wsgi.py

--- inventory_main/wsgi.py
+++ inventory_main/wsgi.py
@@ -8,14 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from settings import base
+
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_main.settings')
-if base.DEBUG:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_main.settings.local')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_main.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_main.settings')
 
 application = get_wsgi_application()
